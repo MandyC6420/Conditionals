005f6d4191b6ebaafa83c406cd039747490fff13
Mandy <drgnfli6420@yahoo.com>
2020-12-09 19:37:32 -0500

    started practice on conditionals chapter

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ if(2 + 2 === 4){
 
 let myName = "Jordan"
 
-if(myName === "Mandy"){
+if(myName === "Mandy" || myName === "Jordan"){
         console.log("Where's the Pizza")
-}else if(myName === "Jordan"){
-        console.log("Hi Jordan")
+// }else if(myName === "Jordan"){
+//         console.log("Hi Jordan")
 }
 else{
     console.log("You should be Mandy!")
@@ -23,9 +23,12 @@ if(isHungry){
 
 let a = 2
 let b = 3
+let c = 4
 
-if(a + b === 5){
+if(a + b === 5 && b + c === 8){
         console.log("You Rock!")
+}else{
+    console.log("bad math")
 }
 
 
@@ -35,8 +38,44 @@ if(a + b === 5){
 let likesToTravel = true
 if(likesToTravel === true){
     console.log("Bon Voyage!")
-}else {
+}
+
+else {
     console.log("Enjoy your Couch!")
 }
 // If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
 
+// Lightning Exercise
+// Fried Pickles are the Best
+// In your JavaScript file, declare a new variable called favoriteFood and set it equal to your favorite food.
+
+let favoriteFood = "Pizza"
+    
+// Write a conditional that checks to see if the favoriteFood variable is equal to "fried pickles" OR "pepperoni rolls". If so, log "Congratulations! You have excellent taste!" to the console. Otherwise, log "Clearly you have not tried fried pickles or pepperoni rolls." to the console.
+if(favoriteFood === "Fried Pickles" || favoriteFood === "Pepperoni Rolls"){
+        console.log("Congratulations! You have excellent taste!")
+}
+else{
+    console.log("Clearly you have not tried fried pickles or pepperoni rolls.")
+}
+
+// Check Your Math
+// Use JavaScript to check and see if the following comparisons are true. For each of the following, log "True fact!" to the console if the condition evaluates to true and "LIES!" to the console if it doesn't.
+// Is the sum of 1 and 1 greater than 5?
+if(1 + 1 >= 5){
+    console.log("True Fact!")
+}else {
+    console.log("LIES!")//should log LIES
+}
+// Is the product of 4 and 5 less than our equal to 20?
+if(4 + 5 <= 20){
+    console.log("True Fact!")//logs true fact
+}else{
+    console.log("LIES!")
+}
+// Is the difference between 6 and 2 greater than or equal to 0?
+if(6-2 >= 0){
+    console.log("True Fact!")//logs true fact
+}else{
+    console.log("LIES!")
+}
