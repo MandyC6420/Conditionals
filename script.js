8b4a420f6387eda8933a9cb71de341b5aa22ce71
Mandy <drgnfli6420@yahoo.com>
2020-12-09 19:05:56 -0500

    else if statements/conditionals

---
+++ script.js
@@ -0,0 +1,42 @@
+//CONDITIONALS
+
+if(2 + 2 === 4){
+        console.log("We can Math!")
+}
+
+let myName = "Jordan"
+
+if(myName === "Mandy"){
+        console.log("Where's the Pizza")
+}else if(myName === "Jordan"){
+        console.log("Hi Jordan")
+}
+else{
+    console.log("You should be Mandy!")
+}
+
+let isHungry = true 
+
+if(isHungry){
+    console.log("You should go eat!")
+}
+
+let a = 2
+let b = 3
+
+if(a + b === 5){
+        console.log("You Rock!")
+}
+
+
+// Lightning Exercise
+// Bon Voyage!
+// In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+let likesToTravel = true
+if(likesToTravel === true){
+    console.log("Bon Voyage!")
+}else {
+    console.log("Enjoy your Couch!")
+}
+// If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
+
